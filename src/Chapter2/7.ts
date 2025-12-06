c14915cedff76937148b1e3943b396b20cf28bbe
punipuni21 <watanaberyotaroyp@gmail.com>
2025-12-06 22:54:10 +0900

    型エラーを意識

---
+++ src/Chapter2/7.ts
@@ -0,0 +1,16 @@
+function getElement(elOrId: string | HTMLElement | null): HTMLElement | HTMLElement {
+	if (elOrId == null) {
+		return document.body;
+	}
+	if (typeof elOrId === "object") {
+		return elOrId;
+	}
+	const el = document.getElementById(elOrId);
+	if (!el) {
+		throw new Error(`Element with id "${elOrId}" not found.`);
+	}
+	return el;
+}
+
+// avoid biome check error
+console.log(getElement(null));
